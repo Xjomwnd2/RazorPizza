df7d8f589891bc2fbba5065f092a0955db281344
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-25 08:48:15 +0300

    commit

---
+++ RazorPizza/RazorPizza/Models/PizzaSize.cs
@@ -0,0 +1,34 @@
+namespace RazorPizza.Models;
+
+public static class PizzaSizes
+{
+    public static Dictionary<string, decimal> SizePriceMultipliers = new()
+    {
+        { "Small", 0.75m },
+        { "Medium", 1.0m },
+        { "Large", 1.35m },
+        { "ExtraLarge", 1.65m }
+    };
+    
+    public static Dictionary<string, string> SizeDescriptions = new()
+    {
+        { "Small", "10\" - 6 slices" },
+        { "Medium", "12\" - 8 slices" },
+        { "Large", "14\" - 10 slices" },
+        { "ExtraLarge", "16\" - 12 slices" }
+    };
+}
+
+// File: CrustTypes.cs
+namespace RazorPizza.Models;
+
+public static class CrustTypes
+{
+    public static Dictionary<string, decimal> CrustPrices = new()
+    {
+        { "Thin", 0.0m },
+        { "Regular", 0.0m },
+        { "Thick", 0.0m },
+        { "Stuffed", 2.0m }
+    };
+}
