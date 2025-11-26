350f18d9f873b17c2df96326c52a4c1300e85efa
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 17:16:39 +0300

    commit

--- RazorPizza/RazorPizza/Data/PizzaDbContext.cs
+++ RazorPizza/RazorPizza/Data/PizzaDbContext.cs
@@ -24,5 +24,38 @@ public class PizzaDbContext : IdentityDbContext<ApplicationUser>
         {
             b.Property(u => u.Id).HasMaxLength(450);
         });
+
+        // Configure decimal precision for all monetary values
+        builder.Entity<Order>(entity =>
+        {
+            entity.Property(e => e.SubTotal).HasPrecision(18, 2);
+            entity.Property(e => e.Tax).HasPrecision(18, 2);
+            entity.Property(e => e.DeliveryFee).HasPrecision(18, 2);
+            entity.Property(e => e.Discount).HasPrecision(18, 2);
+            entity.Property(e => e.TotalAmount).HasPrecision(18, 2);
+        });
+
+        builder.Entity<OrderItem>(entity =>
+        {
+            entity.Property(e => e.Price).HasPrecision(18, 2);
+        });
+
+        builder.Entity<Pizza>(entity =>
+        {
+            entity.Property(e => e.BasePrice).HasPrecision(18, 2);
+        });
+
+        builder.Entity<PromoCode>(entity =>
+        {
+            entity.Property(e => e.DiscountPercent).HasPrecision(5, 2);
+            entity.Property(e => e.DiscountValue).HasPrecision(18, 2);
+            entity.Property(e => e.MaxDiscountAmount).HasPrecision(18, 2);
+            entity.Property(e => e.MinOrderAmount).HasPrecision(18, 2);
+        });
+
+        builder.Entity<Topping>(entity =>
+        {
+            entity.Property(e => e.Price).HasPrecision(18, 2);
+        });
     }
 }
