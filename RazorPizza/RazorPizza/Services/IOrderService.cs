c2c03ca1364f35d2daa698541b16f9d3d1697919
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 05:07:36 +0300

    COMMIT

---
+++ RazorPizza/RazorPizza/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+cat > Services/IOrderService.cs << 'EOF'
+using RazorPizza.Models;
+
+namespace RazorPizza.Services;
+
+public interface IOrderService
+{
+    Task<List<Order>> GetAllOrdersAsync();
+    Task<Order?> GetOrderByIdAsync(int orderId);
+    Task<Order> CreateOrderAsync(Order order);
+    Task UpdateOrderStatusAsync(int orderId, string status);
+}
+EOF
