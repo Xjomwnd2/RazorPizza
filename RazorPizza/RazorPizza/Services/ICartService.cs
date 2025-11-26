c02b9c92711b86fa346ab258591f6206f783c153
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 05:06:19 +0300

    COMMIT

---
+++ RazorPizza/RazorPizza/Services/ICartService.cs
@@ -0,0 +1,15 @@
+cat > Services/ICartService.cs << 'EOF'
+using RazorPizza.Models;
+
+namespace RazorPizza.Services;
+
+public interface ICartService
+{
+    List<CartItem> GetCartItems();
+    void AddToCart(CartItem item);
+    void RemoveFromCart(int pizzaId);
+    void UpdateQuantity(int pizzaId, int quantity);
+    void ClearCart();
+    decimal GetCartTotal();
+}
+EOF
