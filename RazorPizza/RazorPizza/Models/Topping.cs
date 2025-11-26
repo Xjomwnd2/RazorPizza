4fe35795bc3460958bb1cb3fc840a8070ec8106b
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 04:48:56 +0300

    COMIT

---
+++ RazorPizza/RazorPizza/Models/Topping.cs
@@ -0,0 +1,11 @@
+namespace RazorPizza.Models;
+
+public class Topping
+{
+    public int ToppingId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Category { get; set; } = "Other";
+    public bool IsAvailable { get; set; } = true;
+    public string ImageUrl { get; set; } = "/images/toppings/default.jpg";
+}
