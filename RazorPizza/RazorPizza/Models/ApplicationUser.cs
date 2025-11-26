461503cfd1fcdd07d66962e622fe85f580c57625
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 08:50:43 +0300

    commit

--- RazorPizza/RazorPizza/Models/ApplicationUser.cs
+++ RazorPizza/RazorPizza/Models/ApplicationUser.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPizza.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    [MaxLength(450)]
+    public override string Id { get; set; } = Guid.NewGuid().ToString();
+
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-    
+
     // Navigation properties
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
     public virtual ICollection<Address> Addresses { get; set; } = new List<Address>();
