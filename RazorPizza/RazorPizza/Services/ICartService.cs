12c8574b550fa7643262bd576b4e724788eaf459
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 07:05:01 +0300

    COMMIT

--- RazorPizza/RazorPizza/Services/ICartService.cs
+++ RazorPizza/RazorPizza/Services/ICartService.cs
@@ -5,6 +5,7 @@ namespace RazorPizza.Services;
 public interface ICartService
 {
     List<CartItem> GetCartItems();
+    List<CartItem> GetCart(); // Add this alias method
     void AddToCart(CartItem item);
     void RemoveFromCart(int pizzaId);
     void UpdateQuantity(int pizzaId, int quantity);
