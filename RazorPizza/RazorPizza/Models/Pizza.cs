20db50e1611ab313596ef91d8f9965880dcd7900
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 04:46:29 +0300

    COMMIT

--- RazorPizza/RazorPizza/Models/Pizza.cs
+++ RazorPizza/RazorPizza/Models/Pizza.cs
@@ -7,23 +7,9 @@ public class Pizza
     public string Description { get; set; } = string.Empty;
     public decimal BasePrice { get; set; }
     public string ImageUrl { get; set; } = "/images/default-pizza.jpg";
-    public string Category { get; set; } = "Custom"; // Specialty, Custom, Vegetarian, etc.
+    public string Category { get; set; } = "Custom";
     public bool IsAvailable { get; set; } = true;
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     
-    // Navigation properties
     public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
-}
-
-// File: Topping.cs
-namespace RazorPizza.Models;
-
-public class Topping
-{
-    public int ToppingId { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public string Category { get; set; } = "Other"; // Meat, Vegetable, Cheese, Sauce, Other
-    public bool IsAvailable { get; set; } = true;
-    public string ImageUrl { get; set; } = "/images/toppings/default.jpg";
 }
