09ed712560482c6cb4cab93fa7d36229acb4fc0b
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 09:02:42 +0300

    commit

--- RazorPizza/RazorPizza/Program.cs
+++ RazorPizza/RazorPizza/Program.cs
@@ -19,6 +19,10 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPromoCodeService, PromoCodeService>();
 builder.Services.AddScoped<ICartService, CartService>();
 
+// Identity configuration
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<PizzaDbContext>();
+
 // Add session support
 builder.Services.AddSession(options =>
 {
