e2a7d8d89cb9e33f8a807c2a21561d4ff1740be8
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 05:12:45 +0300

    COMMIT

--- Data/PizzaDbContext.cs
+++ Data/PizzaDbContext.cs
@@ -1,13 +1,19 @@
+cat > Data/PizzaDbContext.cs << 'EOF'
+using Microsoft.EntityFrameworkCore;
 using RazorPizza.Models;
 
-namespace RazorPizza.Services;
+namespace RazorPizza.Data;
 
-public interface ICartService
+public class PizzaDbContext : DbContext
 {
-    List<CartItem> GetCartItems();
-    void AddToCart(CartItem item);
-    void RemoveFromCart(int pizzaId);
-    void UpdateQuantity(int pizzaId, int quantity);
-    void ClearCart();
-    decimal GetCartTotal();
+    public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Pizza> Pizzas { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<PromoCode> PromoCodes { get; set; }
+    public DbSet<Topping> Toppings { get; set; }
 }
+EOF
