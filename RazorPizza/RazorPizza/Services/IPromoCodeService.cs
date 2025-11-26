b2d9b3d33e3e26052f3320df12f486032ef0faaf
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-26 06:42:02 +0300

    COMMIT

---
+++ RazorPizza/RazorPizza/Services/IPromoCodeService.cs
@@ -0,0 +1,7 @@
+namespace RazorPizza.Services;
+
+public interface IPromoCodeService
+{
+    Task<PromoCode?> ValidatePromoCodeAsync(string code);
+    decimal ApplyDiscount(decimal amount, PromoCode promoCode);
+}
