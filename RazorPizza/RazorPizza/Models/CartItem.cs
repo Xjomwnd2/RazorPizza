fddf1824422d6c6bdd691588da1350b124197acd
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-28 21:10:20 +0300

    commit

--- RazorPizza/RazorPizza/Models/CartItem.cs
+++ RazorPizza/RazorPizza/Models/CartItem.cs
@@ -1,14 +1,34 @@
-namespace RazorPizza.Models;
-public class CartItem
+using System.Collections.Generic;
+using RazorPizza.Models;
+
+namespace RazorPizza.Models
 {
-    public int PizzaId { get; set; }
-    public string PizzaName { get; set; }
-    public decimal BasePrice { get; set; }
+    public class CartItem
+    {
+        public int CartItemId { get; set; }   // Used in ShoppingCart.razor
+        public int PizzaId { get; set; }
+        public string PizzaName { get; set; }
 
-    // ADD THIS
-    public List<int> SelectedToppingIds { get; set; } = new List<int>();
+        // Base price of the pizza
+        public decimal Price { get; set; }
 
-    public decimal TotalPrice => BasePrice + (SelectedToppings?.Sum(t => t.Price) ?? 0);
+        // Quantity in the cart
+        public int Quantity { get; set; } = 1;
 
-    public List<Topping> SelectedToppings { get; set; } = new();
+        // List of topping IDs selected for this cart item
+        public List<int> SelectedToppingIds { get; set; } = new();
+
+        // Optional list of actual topping objects
+        public List<Topping> SelectedToppings { get; set; } = new();
+
+        // Computed total (pizza + toppings) × quantity
+        public decimal TotalPrice
+        {
+            get
+            {
+                var toppingTotal = SelectedToppings?.Sum(t => t.Price) ?? 0m;
+                return (Price + toppingTotal) * Quantity;
+            }
+        }
+    }
 }
