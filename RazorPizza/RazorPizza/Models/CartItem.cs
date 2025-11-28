5ecb8cd36c0c17d7780a1e90bbf2df6e4facfc4d
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-28 21:27:00 +0300

    commit

--- RazorPizza/RazorPizza/Models/CartItem.cs
+++ RazorPizza/RazorPizza/Models/CartItem.cs
@@ -24,9 +24,7 @@ namespace RazorPizza.Models
         // Toppings used by services
         public List<int> SelectedToppingIds { get; set; } = new();
         public List<Topping> SelectedToppings { get; set; } = new();
-
         // Extra notes from customer
-        public string SpecialInstructions { get; set; }
 
         // Auto-calculated total
         public decimal TotalPrice
